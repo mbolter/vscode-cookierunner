90d8e5c0c450c0cba191951d499aed989ce5bb48
mbolter <mats.bolter@gmail.com>
2020-06-05 12:26:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# vscode-cookierunner README
+
+Extension for running Cookiecutter inside Visual Studio Code.
+
+Activated through Command palette (Ctrl+Shift+P / Command+Shift+P), command "CookieRunner: Create new project" displays templates from predefined local folders and git-repos. Once selected cookiecutter executes with selected user input. Project will be created in currently open VS Code folder.
+
+<img src="images/cmd_run-gif">
+
+## Features
+
+- Run cookiecutter from Command Palette
+- Configure folders containing your cookicutter templates
+- Configure templates in git-repos
+- Cookicutter CLI controlled in Terminal-view
+- Target/Outpur directory is currently open dir
+
+## Requirements
+
+- [Cookiecutter](https://github.com/cookiecutter/cookiecutter)
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `vscode-cookierunner.basic.localDiskTemplates`: Array with local folders containing cookiecutter templates
+* `vscode-cookierunner.basic.gitTemplates`: Array with urls to git repos of templates
+
+## Known Issues
+
+## Release Notes
+
+### 1.0.0
+
+- Initial release
+
+
---
+++ extension.js
@@ -0,0 +1,77 @@
+
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	let disposable = vscode.commands.registerCommand('vscode-cookierunner.cookierunner', function () {
+		var term;
+		
+		var templates = getTemplates()
+		
+		const cookieSelect = () => {
+            return vscode.window.showQuickPick(templates, {
+                placeHolder: "Select Cookiecutter template", ignoreFocusOut: true });
+		};
+		cookieSelect().then(selection => {
+            if (!selection) {
+                return;
+			}
+
+			var outputfolder = vscode.workspace.rootPath
+			var command = "".concat("cookiecutter -o ", outputfolder, " ", selection); 
+			if (vscode.window.terminals.length < 1) {
+				term = vscode.window.createTerminal("bash");
+			} else {
+				term = vscode.window.activeTerminal;
+			}
+			term.show();
+			term.sendText(command);
+
+			vscode.window.showInformationMessage('Running Cookiecutter with : ' + selection);
+		});
+	
+	});
+	context.subscriptions.push(disposable);
+}
+
+function getConfig() {
+	return vscode.workspace.getConfiguration('vscode-cookierunner');
+}
+
+function getLocalTemplateFolders(configuration) {
+	var configuredFolders = configuration.get('basic.localDiskTemplates');
+	var projectFolders = [].concat.apply([], configuredFolders.map(f => getSubFolders(f)));
+	return projectFolders.map(i => i.replace("mats", "testuser"))
+}
+
+function getGitTemplateFolders(configuration) {
+	return configuration.get('basic.gitTemplates')
+}
+
+function getSubFolders(directory) {
+	var subfolders = fs.readdirSync(directory).filter(function (file) {
+		return fs.statSync(directory+'/'+file).isDirectory();
+	  });
+	return subfolders.map(p => path.join(directory, p));
+}
+
+function getTemplates() {
+	var extConfig = getConfig();
+	var localFolders = getLocalTemplateFolders(extConfig);
+	var gitFolders = getGitTemplateFolders(extConfig);
+	var templates = localFolders.concat(gitFolders)
+	return templates
+}
+
+exports.activate = activate;
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
